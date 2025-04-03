3b4596f573eccd6ec66941a5e99ee1beaadef371
Oscar Lesmes <oscarsantiagolesmes@gmail.com>
2025-04-03 18:34:43 -0500

    Pantalla Home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import SportSelectionScreen from './screens/SportSelectionScreen';
 
-function App() {
+const App = () => {
+  const [screen, setScreen] = useState('selection');
+
+  const handleSelectSport = (sport) => {
+    alert(`Deporte seleccionado: ${sport}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {screen === 'selection' && <SportSelectionScreen onSelectSport={handleSelectSport} />}
     </div>
   );
-}
+};
 
 export default App;
